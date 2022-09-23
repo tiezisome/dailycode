d1be669fbb4830494293be2168e961438cbebaea
tiezi <1016657151@qq.com>
2022-09-23 12:39:46 +0800

    proxy.js end

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@
 let person = {
   name: 'jack',
 }
-let proxy = new Proxy(person, {
+let prox = new Proxy(person, {
   get: function (target, propKey) {
     if (propKey in target) {
       return target[propKey]
@@ -14,7 +14,7 @@ let proxy = new Proxy(person, {
   },
 })
 
-console.log('proxy.name=', proxy.name)
+console.log('proxy.name=', prox.name)
 // console.log('proxy.age=', proxy.age)
 
 // get()方法可以继承
@@ -31,3 +31,67 @@ let obj = Object.create(proto)
 console.log('obj.foo=', obj.foo)
 
 // set()
+// 方法用来拦截某个属性的赋值操作，可以接受四个参数，依
+// 次为目标对象、属性名、属性值和 Proxy 实例本身，其中最后一个参数可选
+let validator = {
+  set: function (target, propKey, propValue) {
+    if (propKey == 'age') {
+      if (!Number.isInteger(propValue)) {
+        throw new TypeError('The age is not an Integer')
+      }
+      if (propValue < 0) {
+        throw new Error('the age must > 0')
+      }
+      if (propValue > 200) {
+        throw new RangeError('The age seems invalid')
+      }
+      target[propKey] = propValue
+    }
+  },
+}
+let per = new Proxy({}, validator)
+per.age = 90
+console.log('per.age=', per.age)
+
+//apply()方法 拦截函数的调用、call和apply操作。
+let target = function () {
+  return 'I am the target'
+}
+let handler = {
+  apply: function () {
+    return 'I am the proxy'
+  },
+}
+let p = new Proxy(target, handler)
+console.log('p()=', p())
+
+// 下面这些方法看名字就知道拦截的是啥了
+
+// has()方法用来拦截HasProperty操作
+
+// construct()
+
+// deleteProperty()
+
+// defineProperty()
+
+// getOwnPropertyDescriptor()
+
+// getPrototypeOf()
+
+// isExtensible()
+
+// ownKeys()
+
+// preventExtensions()
+
+// setPrototypeOf()
+
+// Proxy.revocable()返回一个可取消的 Proxy 实例
+target = {}
+handler = {}
+let { proxy, revoke } = Proxy.revocable(target, handler)
+proxy.foo = 123
+proxy.foo // 123
+revoke()
+proxy.foo // TypeError: Revoked
